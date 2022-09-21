de6bf5da24f7e61dc0379ad59286606c512e4074
Yadira Garcia <leyadve@gmail.com>
2022-09-21 15:40:25 -0500

    implentation the Recursion completed

--- index.js
+++ index.js
@@ -1,117 +1,126 @@
-const fs = require('fs') 
-const path = require('node:path')  
-const fetch = require('node-fetch')
-var colors = require('colors');
+const fs = require("fs");
+const path = require("node:path");
+const fetch = require("node-fetch");
+var colors = require("colors");
 
 // RUTA1
-const testRoute = '.\\fileDoc\\prueba1.md'
+const testRoute = ".\\fileDoc\\prueba1.md";
 
- // SABER SI LA RUTA EXISTE
+// SABER SI LA RUTA EXISTE
 const ruteExist = (route) => fs.existsSync(route);
-   
- // SABER SI LA RUTA ES ABSOLUTA
-const getAbsoluteRoute= (route) =>{ // getAbsoluteRoute
+
+// SABER SI LA RUTA ES ABSOLUTA
+const getAbsoluteRoute = (route) => {
+  // getAbsoluteRoute
 
   if (path.isAbsolute(route)) {
-    return route
+    return route;
   }
-  return path.resolve(route) 
-}
+  return path.resolve(route);
+};
 
 // PARA SABER LA EXTENSION
-const ruteExtension = (route) => (path.extname(route));
+const ruteExtension = (route) => path.extname(route);
 
 // PARA EXTRAER LON LINK DEL ARCHIVO MD
-const getLinks = (route)  => {
+const getLinks = (route) => {
   const arrayLinks = [];
 
   // PARA LEER EL ARCHIVO
-  const ruteRead = fs.readFileSync(testRoute, 'utf8');
- 
-  if (ruteRead !== ''){
+  const ruteRead = fs.readFileSync(testRoute, "utf8");
+
+  if (ruteRead !== "") {
     const urlFind = ruteRead.match(/\[.*\]\(.*\)/gm);
-  
-    if(urlFind !== 'null'){
-      urlFind.map( (url) => { 
-      const text = url.slice(1, url.indexOf(']'));
-      const href = url.slice(url.indexOf(']') +2, url.indexOf(')'));
-      const routeF = __dirname + route;
-      const objLiks ={
-        href,
-        text,
-        routeF,
-      }
-         arrayLinks.push(objLiks);
-     })
+
+    if (urlFind !== "null") {
+      urlFind.map((url) => {
+        const text = url.slice(1, url.indexOf("]"));
+        const href = url.slice(url.indexOf("]") + 2, url.indexOf(")"));
+        const routeF = __dirname + route;
+        const objLiks = {
+          href,
+          text,
+          routeF,
+        };
+        arrayLinks.push(objLiks);
+      });
     }
   }
   return arrayLinks;
-}
-
+};
 
 // FUNCION VALIDATE
- const validateStatus = (arrayLinks) => {
+const validateStatus = (arrayLinks) => {
   newArrayPromises = [];
   arrayLinks.forEach((objL) => {
-    const httpLink = objL.href
+    const httpLink = objL.href;
     // console.log(httpLink)
     const promise = fetch(httpLink)
-    .then((promiseFetch) =>{
-      return {
-        // href: objL.href,
-        // text: objL.text,
-        ...objL,
-        linksStatus: promiseFetch.status,
-        message: 'ok'
-      }
-    })
-    .catch((err) => {
-     
-      return {
-        ...objL,
-        message: 'fail'
-      }
-    });
+      .then((promiseFetch) => {
+        return {
+          // href: objL.href,
+          // text: objL.text,
+          ...objL,
+          linksStatus: promiseFetch.status,
+          message: "ok",
+        };
+      })
+      .catch((err) => {
+        console.log(err)
+        return {
+          ...objL,
+          message: "fail",
+        };
+      });
     newArrayPromises.push(promise);
-   
-   });
-   return newArrayPromises
- };
+  });
+  return newArrayPromises;
+};
 
-// FUNCION STATS 
+// FUNCION STATS
 const stats = (newArrayPromises) => {
-
-const total = newArrayPromises.length;
-const unique = newArrayPromises.filter(objLi => objLi.message === 'ok').length;
-const broken = newArrayPromises.filter(objLi => objLi.message === 'fail').length;
-  return{
+  const total = newArrayPromises.length;
+  const unique = newArrayPromises.filter(
+    (objLi) => objLi.message === "ok"
+  ).length;
+  const broken = newArrayPromises.filter(
+    (objLi) => objLi.message === "fail"
+  ).length;
+  return {
     total,
     unique,
     broken,
-  }; 
+  };
 };
 
-
 // RECORRER DIRECTORIO, RECURSIVIDAD
-const directory = './fileDoc'
-  const findFileInDir = (directory)=> {
-    if(fs.statSync(directory).isDirectory()){
-      console.log('hola')
-      const dirSearch = fs.readdirSync(directory)
 
-      dirSearch.forEach((files) =>{
-        console.log(files)
-       findFileInDir(files);
-       
+const routeDirectory = path.join(__dirname + "/fileDoc");
+
+const findFileInDirectory = (routeDirectory) => {
+  const arrayFiles = [];
+
+  const readDirectory = fs.readdirSync(routeDirectory);
+  readDirectory.forEach((files) => {
+    const newRoutedirectory = path.join(routeDirectory, files);
+ 
+    if (fs.statSync(newRoutedirectory).isDirectory()) {
+   
+      findFileInDirectory(newRoutedirectory).forEach((file) => {
+        arrayFiles.push(file);
       })
-    }else{
-      console.log('no es directorio');// pregunta si existe directorio
+    }
+    //if(ruteExtension(newRoutedirectory) === '.md')
+    else {
+      //console.log(newRoutedirectory)
+      arrayFiles.push(newRoutedirectory);
+    }
+  });
+  return arrayFiles;
+  //console.log('no es directorio');// pregunta si existe directorio
+};
 
- }
-}
-
-findFileInDir(directory);
-
+console.log(findFileInDirectory(routeDirectory))
 
 
 // PROMESA
@@ -119,32 +128,28 @@ const mdLink = (route) =>
   new Promise((resolve, reject) => {
     ruteExist(route);
     getAbsoluteRoute(route);
-    ruteExtension (route);
+    ruteExtension(route);
 
-    const arrayLinks = getLinks(route) 
+    const arrayLinks = getLinks(route);
     const arrayPromises = validateStatus(arrayLinks);
 
-    Promise.all(arrayPromises).then(res => {
-        // console.log('arriba',res)
+    Promise.all(arrayPromises).then((res) => {
+      // console.log('arriba',res)
       stats(res);
-      // console.log(stats(res)) 
+      // console.log(stats(res))
     });
-  
-    resolve([]);
- });
 
-mdLink(testRoute)
-  .then((result) =>{ 
-    // console.log(result) 
+    resolve([]);
   });
 
+mdLink(testRoute).then((result) => {
+  // console.log(result)
+});
 
- module.exports ={
+module.exports = {
   ruteExist,
   getAbsoluteRoute,
-  ruteExtension ,
+  ruteExtension,
   getLinks,
   validateStatus,
- };
-
-
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,104 +2,98 @@
 const {
   ruteExist,
   getAbsoluteRoute,
-  ruteExtension ,
+  ruteExtension,
   getLinks,
   validateStatus,
-} = require('../index.js');
+} = require("../index.js");
 
-const testRoute = '.\\fileDoc\\prueba1.md'
-const testRouteFalse = '.\fileDoc\prueba1.md'
-const testRouteAbs = 'D:\\LABORATORIA\\LIM018-md-links\\fileDoc\\prueba1.md';
+const testRoute = ".\\fileDoc\\prueba1.md";
+const testRouteFalse = ".\fileDocprueba1.md";
+const testRouteAbs = "D:\\LABORATORIA\\LIM018-md-links\\fileDoc\\prueba1.md";
 
 const testArrayLinks = [
   {
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
-    routeF: 'D:\\LABORATORIA\\LIM018-md-links.\\fileDoc\\prueba1.md'
-  }
-  ];
+    href: "https://es.wikipedia.org/wiki/Markdown",
+    text: "Markdown",
+    routeF: "D:\\LABORATORIA\\LIM018-md-links.\\fileDoc\\prueba1.md",
+  },
+];
 
 describe("ruteExist", () => {
-  it('is a function', () => {
-    expect(typeof ruteExist).toBe('function');
+  it("is a function", () => {
+    expect(typeof ruteExist).toBe("function");
   });
 
-  it('deberia confirmar si existe la ruta', ()=>{
-    expect(ruteExist(testRoute)).toEqual(true)
+  it("deberia confirmar si existe la ruta", () => {
+    expect(ruteExist(testRoute)).toEqual(true);
   });
 
-  it('deberia confirmar si NO existe la ruta', ()=>{
-    expect(ruteExist(testRouteFalse)).toEqual(false)
-  })
+  it("deberia confirmar si NO existe la ruta", () => {
+    expect(ruteExist(testRouteFalse)).toEqual(false);
+  });
 });
 
-
 describe("ruteIsAbsolute", () => {
-  it('is a function', () => {
-    expect(typeof getAbsoluteRoute).toBe('function');
+  it("is a function", () => {
+    expect(typeof getAbsoluteRoute).toBe("function");
   });
 
-  it('deberia confirmar si la ruta es Absoluta, sino convertirla a Absoluta', ()=>{
+  it("deberia confirmar si la ruta es Absoluta, sino convertirla a Absoluta", () => {
     expect(getAbsoluteRoute(testRoute)).toEqual(testRouteAbs);
-  })
+  });
 
-  it('deberia devolverla ruta es Absoluta, prque se recibio como tal', ()=>{
+  it("deberia devolverla ruta es Absoluta, prque se recibio como tal", () => {
     expect(getAbsoluteRoute(testRouteAbs)).toEqual(testRouteAbs);
-  })
-
+  });
 });
 
 describe("ruteExtension", () => {
-  it('is a function', () => {
-    expect(typeof ruteExtension).toBe('function');
+  it("is a function", () => {
+    expect(typeof ruteExtension).toBe("function");
   });
 
-  it('deberia confirmar si existen archivos .md', ()=>{
-    expect(ruteExtension(testRoute)).toEqual('.md');
-    ;
-  })
+  it("deberia confirmar si existen archivos .md", () => {
+    expect(ruteExtension(testRoute)).toEqual(".md");
+  });
 });
 
 describe("getLinks", () => {
-  it('is a function', () => {
-    expect(typeof getLinks).toBe('function');
+  it("is a function", () => {
+    expect(typeof getLinks).toBe("function");
   });
 
-  it('deberia obtener un array de links', ()=>{
+  it("deberia obtener un array de links", () => {
     expect(getLinks(testRoute)).toEqual(testArrayLinks);
-  })
+  });
 });
 
 describe("validateStatus", () => {
-  it('is a function', () => {
-    expect(typeof validateStatus).toBe('function');
+  it("is a function", () => {
+    expect(typeof validateStatus).toBe("function");
   });
 
   const promiseTestArray = [
     {
-      href: 'https://es.wikipedia.org/wiki/Markdown',
-      text: 'Markdown',
-      routeF: 'D:\\LABORATORIA\\LIM018-md-links.\\fileDoc\\prueba1.md',
+      href: "https://es.wikipedia.org/wiki/Markdown",
+      text: "Markdown",
+      routeF: "D:\\LABORATORIA\\LIM018-md-links.\\fileDoc\\prueba1.md",
       linksStatus: 200,
-      message: 'ok'
-    }
-    ];
+      message: "ok",
+    },
+  ];
 
-  it('deberia obtener un array de links', ()=>{
-    return expect(validateStatus(testArrayLinks)).resolves.toEqual(promiseTestArray);
-    })
-  })
+  it("deberia obtener un array de links", () => {
+    return expect(validateStatus(testArrayLinks)).resolves.toEqual(
+      promiseTestArray
+    );
+  });
 
-  it('deberia obtener un array de links', ()=>{
-    return validateStatus(testArrayLinks).then(result =>{
-      expect(result).toBe(promiseTestArray)
-    })
-    })
- 
-
-
-
-
+  it("deberia obtener un array de links", () => {
+    return validateStatus(testArrayLinks).then((result) => {
+      expect(result).toBe(promiseTestArray);
+    });
+  });
+});
 
 // describe('mdLink', () => {
 
